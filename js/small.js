009036541caa7c73ecf4cf62cd9ec1bd11cb6a0e
dyep49 <dyep49@gmail.com>
2014-06-05 18:50:52 -0400

    parse out hash in url

--- js/small.js
+++ js/small.js
@@ -3,9 +3,9 @@ var urlArray = ["http://www.businessweek.com/articles/2014-06-04/chinas-xiaomi-t
 var thisUrl;
 
 if(urlArray.indexOf(window.top.location.href) != -1) {
-  thisUrl = window.top.location.href
+  thisUrl = window.top.location.href.split('#')[0]
 } else if (urlArray.indexOf(document.referrer) != -1) {
-  thisUrl = document.referrer
+  thisUrl = document.referrer.split('#')[0]
 } else {
   thisUrl = _.shuffle(urlArray)[0]  
 }
